906b2d37e1d1436617b6fc419b6efb1fc20b2e09
andrehideki <andre.hkiw@gmail.com>
2021-10-03 11:52:03 -0300

    add: index.html -> form to send files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const PORT = 3333;
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('hello world');
+  res.sendFile(path.join(__dirname, '/index.html'))
 });
+
 server.listen(PORT, () => console.log(`Application running at: ${PORT}`));
