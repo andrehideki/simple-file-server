d2bd44891de8cb5996b0531db79ec7789a34ff7c
andrehideki <andre.hkiw@gmail.com>
2021-10-03 12:12:17 -0300

    add: receiving files

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 
 const PORT = 3333;
 const server = express();
 
+server.use(fileUpload());
+
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
+});
+
+server.post('/upload', (req, res) => {
+  const files = req.files;
+  console.log('here', files);
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
 });
 
 server.listen(PORT, () => console.log(`Application running at: ${PORT}`));
